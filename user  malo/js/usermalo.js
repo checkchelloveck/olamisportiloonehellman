b6bcff31aa7a62eb042fe1910178df689baff7c2
Malo Sourse <117565248+checkchelloveck@users.noreply.github.com>
2023-02-06 16:39:21 +0200

    malo olam site

---
+++ user  malo/js/usermalo.js
@@ -0,0 +1,25 @@
+//перед записью "use strict" допустим только коментарий
+"use strict"
+//коментарий строгий режим
+//coment
+
+/*либо вот таким образом
+ставиться коментарий 
+*/
+
+console.log('WELCOM TO PLANET MALO ');
+
+console.log(' go to javascript');
+
+//коментарий
+
+//создаем переменную
+
+let mylifestyle;
+
+//присваеваем значение переменной
+
+mylifestyle = 'frilanse';
+mylifestyle = 18;
+//выводимм в консоль значение переменной
+console.log(mylifestyle);
